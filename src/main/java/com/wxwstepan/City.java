d7cbdcd401470705d7f5816b798f6e04c9787861
WxW <polyakovs@list.ru>
2022-10-27 15:48:14 +0300

    complete first lesson

---
+++ src/main/java/com/wxwstepan/City.java
@@ -0,0 +1,30 @@
+package com.wxwstepan;
+
+public class City {
+    int id;
+    String name;
+    String region;
+    String district;
+    int population;
+    String foundation;
+
+    public City(int id, String name, String region, String district, int population, String foundation) {
+        this.id = id;
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation=" + foundation +
+                '}';
+    }
+}
--- src/main/java/com/wxwstepan/Main.java
+++ src/main/java/com/wxwstepan/Main.java
@@ -1,7 +1,35 @@
 package com.wxwstepan;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(Paths.get("city_ru.csv"));
+        scanner.useDelimiter(System.getProperty("line.separator"));
+        while(scanner.hasNext()){
+            City city = parseLine(scanner.next());
+            System.out.println(city);
+        }
+        scanner.close();
+
+    }
+
+    private static City parseLine(String line) {
+        String[] cityArray = line.split(";");
+        String idString = cityArray[0];
+        String name = cityArray[1];
+        String region = cityArray[2];
+        String district = cityArray[3];
+        String populationString = cityArray[4];
+        String foundation = null;
+        if (cityArray.length == 6) {
+            foundation = cityArray[5];
+        }
+        int id = Integer.parseInt(idString);
+        int population = Integer.parseInt(populationString);
+        return new City(id,name,region,district,population,foundation);
     }
 }
